a378353ac60cfa5e902a1baedb4dff0bd2681817
yangjunfei-lotus <junfei.yang@lotusflare.com>
2021-07-12 16:12:20 +0800

    完成Robolectric使用的首个范例

--- app/src/main/java/com/lotusflare/robolectricpractice/MainActivity.kt
+++ app/src/main/java/com/lotusflare/robolectricpractice/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.lotusflare.robolectricpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun changeWelcomeText(view: View) {
+        findViewById<TextView>(R.id.welcomeTextView).text = "你好，jephy"
     }
 }
